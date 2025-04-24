7225ae70558b3446266954fce0336eb9616f36b1
DGwebdes <alan.bilik8@gmail.com>
2025-04-24 04:03:04 +0100

    finished lesson 14. next, authentication

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import Form from "@/app/ui/invoices/create-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers } from "@/app/lib/data";
 
-const Create = () => {
-    return <div>Create</div>;
-};
+export default async function Page() {
+    const customers = await fetchCustomers();
 
-export default Create;
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: "Invoices", href: "/dashboard/invoices" },
+                    {
+                        label: "Create Invoice",
+                        href: "/dashboard/invoices/create",
+                        active: true,
+                    },
+                ]}
+            />
+            <Form customers={customers} />
+        </main>
+    );
+}
